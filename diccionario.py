c12f81231ea9be251a3de1aa8035450fdb81df5b
ivdejel castrillon ruiz <ivdejelcastrillon@colegioguanenta.edu.co>
2026-04-22 07:09:06 -0500

    Add files via upload

--- diccionario.py
+++ diccionario.py
@@ -4,7 +4,8 @@ for i in range(3):
     nombre = input("Nombre: ").upper()
     telefono = input("telefono: ")
     agenda[nombre] = telefono
-
+    if nombre in agenda:
+        print(f"{nombre} {agenda[nombre]}")
 # consultar
 buscar = input("Buscar nombre. ").upper()
 print(agenda.get(buscar, "No exixte"))
@@ -12,6 +13,7 @@ print(agenda.get(buscar, "No exixte"))
 # Eliminar
 eliminar = input("Eliminar nombre. ").upper()
 agenda.pop(eliminar, None)
+
 
 #Mostrar agenda inal
 print(agenda)
